5ca656d097a465e25e7b5b7f7ef05cde04d8cf24
Reece_Collett <rcollett@spartaglobal.com>
2018-08-09 15:15:35 +0100

    Created function to roll dice and refresh

--- dice-starter-code/js/index.js
+++ dice-starter-code/js/index.js
@@ -1,6 +1,53 @@
-function rollDice() {
-  return Math.floor(Math.random() * 6)
+function randomNumberGen() {
+  return Math.floor(Math.random() * 6) + 1 // Generates the random number between 1 and 6
 }
 
-var randNum = rollDice();
-debugger;
+function rollDice(randNum) {
+
+  var innerSquare = document.querySelectorAll("div.innerSquare");
+  var newClass = "innerSquare dot";
+
+  for (var i = 0; i < innerSquare.length; i++) {
+    innerSquare[i].className = "innerSquare";
+  }
+  switch (randNum) {
+    case 1: // When randNum is 1, change the class name to newClass
+      innerSquare[4].className = newClass;
+      break;
+
+    case 2:
+      innerSquare[0].className = newClass;
+      innerSquare[8].className = newClass;
+      break;
+
+    case 3:
+      innerSquare[0].className = newClass;
+      innerSquare[4].className = newClass;
+      innerSquare[8].className = newClass;
+      break;
+
+    case 4:
+      innerSquare[0].className = newClass;
+      innerSquare[2].className = newClass;
+      innerSquare[6].className = newClass;
+      innerSquare[8].className = newClass;
+      break;
+
+    case 5:
+      innerSquare[0].className = newClass;
+      innerSquare[2].className = newClass;
+      innerSquare[4].className = newClass;
+      innerSquare[6].className = newClass;
+      innerSquare[8].className = newClass;
+      break;
+
+    case 6:
+      innerSquare[0].className = newClass;
+      innerSquare[2].className = newClass;
+      innerSquare[3].className = newClass;
+      innerSquare[5].className = newClass;
+      innerSquare[6].className = newClass;
+      innerSquare[8].className = newClass;
+      break;
+    }
+}
